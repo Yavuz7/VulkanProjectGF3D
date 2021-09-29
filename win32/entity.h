aa0fc1410996e92ac0a4ae11b5b7a00ba7393df2
Yavuz Yurt <ysy5@njit.edu>
2021-09-29 09:48:58 -0400

    Finished entity class

---
+++ win32/entity.h
@@ -0,0 +1,62 @@
+#ifndef ENTITY_H_
+#define ENTITY_H_
+
+#include "gfc_types.h"
+#include "gf3d_model.h"
+
+typedef struct Entity_S{
+	Uint8 _inuse;/*Keeps track of memory Usage*/
+	Model *model;/*pointer to entity to draw*/
+	void(*think)();/*pointer to think function*/
+	//etc
+}Entity;
+
+Matrix4 modelmat; //animation matrixfor model
+
+/*
+*@Brief Intializes Entity
+*@Param MaxEntities
+*/
+
+void entity_system_init(Uint8 maxEntities); //Initializes entity
+
+/*Pointer to new entity*/
+
+Entity *entity_new();
+
+/*
+*@Brief Frees Entities
+*@Param Entity being freed
+*/
+
+void entity_free(Entity *self);
+
+/*
+*@Brief Draws Entities
+*@Param Entity being drawn
+*/
+
+void entity_draw(Entity *self);
+
+/*
+*@Brief Entity Logic
+*@Param Entity using Logic
+*/
+
+void entity_think(Entity *self);
+
+/*
+*@Brief Draw entities 
+*@Param 
+*/
+
+void entity_draw_all();
+
+/*
+*@Brief Entities Act
+*@Param 
+*/
+
+void entity_think_all();
+
+#endif
