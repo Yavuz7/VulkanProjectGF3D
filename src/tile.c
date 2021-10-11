adaa0a5cfdf3ec15d806b14f0368dccd087bc5cc
Yavuz Yurt <ysy5@njit.edu>
2021-10-11 13:13:44 -0400

    Tile system now loads from file

--- src/tile.c
+++ src/tile.c
@@ -10,7 +10,8 @@ Tile tileMap[6][6]; /*Array to store tile map in*/
 void loadMap(char *mapData)
 {
 	FILE *map;
-	char buff[255];
+	char buff[256];
+	int x, y;
 	if (!mapData)
 	{
 		slog("Didn't get mapData file parameter ,w,");
@@ -26,41 +27,13 @@ void loadMap(char *mapData)
 	}
 	slog("Map Opened ^w^");
 	slog_sync();
-	return;
-/*	smap = sj_string_to_json_string(map);
-	if (!smap)
-	{
-		slog("Didn't convert the json to string, ,w, ");
-		slog_sync();
-		return;
-	}
-	
+
 	for (x = 0; x <= 6; x++)
 	{
 		for (y = 0; y <= 6; y++)
 		{
-			textPlace += 1;
-			if (smap->text[textPlace] == ",")
-			{
-				do
-				{
-					textPlace += 1;
-					if (textPlace > 98)
-					{
-						slog("Too many numbers scanned through owo");
-						slog_sync();
-						return;
-					}
-				} while (smap->text[textPlace] == ',');
-			}
-
-			tileMap[x][y]._tileType = smap->text[textPlace];
-			
-			if (tileMap[x][y]._tileType > 10)
-			{
-				slog("Tile was set above ten, which means something that wasn't a number was read");
-				slog_sync();
-			}
+			fscanf(map,"%i", buff);
+			tileMap[x][y]._tileType = buff[0];
 			
 			slog("Tile %d , %d set to Type %i", x, y, tileMap[x][y]._tileType);
 			slog_sync();
@@ -76,5 +49,5 @@ void drawTiles(){
 		{
 
 		}
-	}*/
+	}
 }
